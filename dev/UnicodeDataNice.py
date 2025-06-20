144d3182604e7eba31dc3a37ea4ce7ab229e47a7
David Richards <rawdod@gmail.com>
2025-06-20 01:23:49 -0400

    nice

--- dev/UnicodeDataNice.py
+++ dev/UnicodeDataNice.py
@@ -3,7 +3,7 @@ with open("UnicodeData.txt", "r") as file:
     parts = line.strip().split(";")
     number = int(parts[0], 16)
     numberstr = str(number)
-    numberstr = numberstr.rjust(7, '0')
+    numberstr = numberstr.rjust(7, ' ')
     name = parts[1]
     if (parts[1] == "<control>"): name = parts[10]
     print(numberstr, name)
